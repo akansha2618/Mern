d7cb1bf5d2ede84a9a0227ab161ca162cda02331
akansha2618 <akanranamvlu@gmail.com>
2026-07-15 12:27:40 +0530

    Create Practical_3e1_App.jsx

---
+++ Practical_3e1_App.jsx
@@ -0,0 +1,12 @@
+import Student from"./components/Student";
+function App(){
+  return(
+  <div>
+    <h1>This is Function component</h1>
+    <hr/>
+    <Student/>
+    <hr/>
+  </div>
+  );
+}
+export default App;
